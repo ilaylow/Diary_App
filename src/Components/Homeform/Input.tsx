a910d3395283cb88117b7cfdc16bc3cc16144b5c
ilaylow <chuenleylow@gmail.com>
2021-07-30 21:10:27 +1000

    added in task page front end

--- src/Components/Homeform/Input.tsx
+++ src/Components/Homeform/Input.tsx
@@ -7,24 +7,25 @@ interface InputProps {
     name: string;
     label: string;
     autoFocus: boolean;
-    half: boolean;
+    half?: boolean;
     handleChange: (event: any) => void;
     handleShowPassword?: (event: any) => void;
+    variant?: "outlined" | "standard" | "filled" | undefined;
     type?: string;
+    style?: string;
 }
 
-
-
-export const Input: React.FC<InputProps> = ({name, label, autoFocus, half, handleChange, type, handleShowPassword}) => {
+export const Input: React.FC<InputProps> = ({name, label, autoFocus, half = false, handleChange, type, handleShowPassword, variant= "outlined", style}) => {
     return (
         <Grid item xs = {12} sm = {half ? 6 : 12}>
             <TextField
             name = {name}
             onChange={handleChange}
-            variant = "outlined"
+            variant = {variant}
             required
             fullWidth
             label = {label}
+            className = {style}
             autoFocus = {autoFocus}
             type = {type}
             InputProps = {(name === "password") ? {
--- src/Components/Taskpage/index.tsx
+++ src/Components/Taskpage/index.tsx
@@ -1,16 +1,69 @@
 import React from "react";
-import {Container, Paper, Typography} from "@material-ui/core";
+import {Grid, Container, makeStyles, Paper, Typography, TextField} from "@material-ui/core";
+import { Input } from '../Homeform/Input';
+import { useState }from "react";
 
 interface Props{
 
 }
 
+const font1 = "Indie Flower, cursive";
+
+const useStyles = makeStyles((theme) => ({
+    container:{
+        float: "left",
+        textAlign: "center",
+        marginLeft: "3%",
+        border: "solid white",
+        marginTop: "10%",
+    },
+
+    paper:{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: "auto",
+        padding: theme.spacing(1),
+        width: "80%",
+    },
+    form: {
+        marginTop: "5%",
+        width: "50%"
+    },
+    
+    inputField1: {
+        width: "80%",
+        float: "left",
+        marginLeft: "5%",
+        fontFamily: font1,
+    }
+}));
+
+const initialTask = {title : "", prority: "", description: ""}
+
 export const Taskpage : React.FC<Props> = ({}) => {
+    const styles = useStyles();
+
+    const [taskState, setTaskState] = useState(initialTask);
+
+    const handleInput = (e) => {
+        setTaskState({...taskState, [e.target.name]: e.target.value})
+    }
+
     return (
-        <Container component = "main" maxWidth = "xs">
-            <Paper elevation = {3}>
-                <Typography component = "h1" variant = "h5">What are you planning for today?</Typography>
-            </Paper>
-        </Container>
+        <div style = {{border: "solid white"}}>
+            <Container component = "main" maxWidth = "md" className = {styles.container}>
+                <Paper elevation = {3} className = {styles.paper}>
+                    <Typography style = {{fontFamily: font1, fontWeight: "bold"}} 
+                    component = "h1" 
+                    variant = "h5">What are you planning for today?</Typography>
+                    <form className = {styles.form}>
+                        <Grid container spacing = {3}>
+                            <Input name = "title" label = "Title" autoFocus  handleChange = {handleInput} variant = "standard" style = {styles.inputField1}/>
+                        </Grid>
+                    </form>
+                </Paper>
+            </Container>
+        </div>
     )
 }
